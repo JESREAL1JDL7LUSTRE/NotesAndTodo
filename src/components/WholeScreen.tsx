bf39ed15055077346055f1317d4e020c83dddbdd
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2025-01-30 12:04:36 +0800

    Finish??

--- src/components/WholeScreen.tsx
+++ src/components/WholeScreen.tsx
@@ -1,13 +1,36 @@
-import Note from "./Note"
-import Todo from "./Todo"
+import { useState } from 'react';
+import Note from "./Note";
+import Todo from "./Todo";
 
 const WholeScreen = () => {
+  const [activePanel, setActivePanel] = useState<'todo' | 'note'>('todo');
+
   return (
-    <div id="container" className=" flex justify-center w-full items-center h-screen bg-sky-400">
-      <Note />
-      <Todo />
+    <div className="flex h-screen w-full bg-sky-400 overflow-hidden relative">
+      <div className="">
+      </div>
+      <div
+        className={`absolute w-full h-full transition-transform duration-300 cursor-pointer ${
+          activePanel === 'todo' 
+            ? 'translate-x-0 z-10' 
+            : '-translate-x-[70%] z-20'
+        }`}
+        onClick={() => setActivePanel('todo')}
+      >
+        <Note />
+      </div>
+      <div
+        className={`absolute w-full h-full transition-transform duration-300 cursor-pointer ${
+          activePanel === 'note' 
+            ? 'translate-x-0 z-10' 
+            : 'translate-x-[70%] z-20'
+        }`}
+        onClick={() => setActivePanel('note')}
+      >
+        <Todo />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default WholeScreen
